275a58304751d4a60d6b4ad6a09d109dd7c42724
Vansh=Narang <vanshnarang49906@gmail.com>
2023-04-22 13:04:20 +0530

    Modal Bottom Sheet added

--- flutter_application_33/lib/map/MapScreen.dart
+++ flutter_application_33/lib/map/MapScreen.dart
@@ -3,12 +3,14 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:custom_marker/marker_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_33/map/sample/xyz.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -19,6 +21,7 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   Set<Marker> markers = {};
+  TextEditingController _date = TextEditingController();
   late GoogleMapController mapController;
   var currlocation;
   var clients = [];
@@ -52,37 +55,67 @@ class _MapScreenState extends State<MapScreen> {
             position: LatLng(querySnapshot.docs[i]['location'].latitude,
                 querySnapshot.docs[i]['location'].longitude),
             onTap: () {
+              const xyz();
+              // );
               showModalBottomSheet(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
                 context: context,
                 builder: (context) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Container(
-                              child: Text(
-                                querySnapshot.docs[i]['category'],
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            Container(
-                              child: Text(
-                                "Contact Vendor" +
-                                    querySnapshot.docs[i]['contactno'],
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
+                  return Column(
+                    children: [
+                      SizedBox(height: 10),
+                      ListTile(
+                        onTap: () {
+                          print("called");
+                        },
+                        leading: Icon(Icons.call),
+                        title: Text("Contact"),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: TextField(
+                          keyboardType: TextInputType.none,
+                          controller: _date,
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.calendar_today_rounded),
+                            labelText: "Select Date",
+                          ),
+                          onTap: () async {
+                            DateTime? pickDate = await showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2101));
+
+                            if (pickDate != null) {
+                              setState(() {
+                                _date.text =
+                                    DateFormat('dd-MM-yyyy').format(pickDate);
+                              });
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Wrap(children: [
+                                  AlertDialog(
+                                    title: Text("Order Done Successfully 🎉"),
+                                  ),
+                                ]);
+                              },
+                            );
+                          },
+                          child: Text("Order Now")),
+                    ],
                   );
                 },
               );
@@ -109,6 +142,7 @@ class _MapScreenState extends State<MapScreen> {
           ),
           markers: Set<Marker>.from(markers)
           // Marker(
+          //custom marker
           //     markerId: MarkerId("src"),
           //     position: source,
           //     draggable: true,
